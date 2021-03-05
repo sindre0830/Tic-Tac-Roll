08d0430aa46384d57f35efd78211c1887394515b
Sindre Eiklid <sindre0830@gmail.com>
2021-03-05 11:01:41 +0100

    Added new types and a newBoard function

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,4 +1,5 @@
 # Changelog for assignment-one
 [0.1.0.0] setup: Initial commit
+[0.1.0.1] development: Added new types and a newBoard function
 
 ## Unreleased changes
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,30 @@
 module Lib
-    ( someFunc
-    ) where
+	( someFunc
+	) where
+
+import System.Environment
+import Data.List
+
+type BoardSize = Int
+
+boardSize :: BoardSize
+boardSize = 3
+
+data Move = X | O
+data Cell = Occupied Move | Empty
+
+instance Show Move where
+	show X = "X"
+	show O = "O"
+
+instance Show Cell where
+	show (Occupied X)     = "X"
+	show (Occupied O)     = "O"
+	show Empty            = " "
+
+newBoard :: [Cell]
+newBoard = [Empty | i <- [1..(boardSize * boardSize)]]
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+	print newBoard
